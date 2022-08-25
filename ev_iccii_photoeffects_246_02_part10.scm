0b30e198069bcefcd2d5c61a0496b0a93309a8e6
Karl <63458781+karlhof26@users.noreply.github.com>
2022-08-25 09:15:16 +0100

    Use Gimpress defaults. Removed dependency

--- ev_iccii_photoeffects_246_02_part10.scm
+++ ev_iccii_photoeffects_246_02_part10.scm
@@ -1,8 +1,27 @@
+; The GIMP -- an image manipulation program   
+; Copyright (C) 1995 Spencer Kimball and Peter Mattis 
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 3 of the License, or
+; (at your option) any later version.
+; 
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+; 
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+; http://www.gnu.org/licenses/gpl-3.0.html
+;
+;
 ; 
 ;*************************************************************************************** 
 ; Palette Knife image script  for GIMP 2.2
 ; Copyright (C) 2007 Eddy Verlinden <eddy_verlinden@hotmail.com>
-; 
+; Uodated by karlhof26 to use best Gimpressionist default - not quite palette knife but no dependencies. Uzses Wormcan.
 ; --------------------------------------------------------------------
 
 
@@ -32,7 +51,7 @@
     (gimp-edit-copy drawable)
     (gimp-floating-sel-anchor (car (gimp-edit-paste layer-temp1 0)))
     
-    (plug-in-gimpressionist 1 img layer-temp1 "ev_paletknife2")
+    (plug-in-gimpressionist 1 img layer-temp1 "Wormcan")
     (gimp-drawable-levels layer-temp1 HISTOGRAM-VALUE 0.0 1.0 TRUE 0.5 0.0 1.0 TRUE) 
     
     (gimp-selection-load old-selection)
@@ -56,7 +75,7 @@
 (script-fu-register
     "script-fu-paletteknife-ga"
     "<Toolbox>/Script-Fu/Decor/Photo Effects/Artist/Palette Knife..."
-    "Creates a drawing effect like made with a palette knife, based on the Gimpressionist. \n: file:photoeffects_246_02_part10.scm"
+    "Creates a drawing effect like made with a palette knife, based on the Gimpressionist. Uses Wormcan option.\n: file:photoeffects_246_02_part10.scm"
     "Eddy Verlinden <eddy_verlinden@hotmail.com>"
     "Eddy Verlinden"
     "2007, juli"
@@ -97,7 +116,7 @@
     (gimp-edit-copy drawable) 
     (gimp-floating-sel-anchor (car (gimp-edit-paste layer-temp1 0)))
     
-    (plug-in-gimpressionist 1 img layer-temp1 "ev_angledstrokes")
+    (plug-in-gimpressionist 1 img layer-temp1 "Line-art-2") ; was ev_angeledstrokes
     (plug-in-unsharp-mask 1 img layer-temp1 5.0 1.0 0) 
     
     (gimp-selection-load old-selection)
@@ -121,7 +140,7 @@
 (script-fu-register
     "script-fu-angledstrokes-ga"
     "<Toolbox>/Script-Fu/Decor/Photo Effects/Brush/Angled strokes..."
-    "Creates a drawing effect, based on the Gimpressionist. \n: file:photoeffects_246_02_part10.scm"
+    "Creates a drawing effect, based on the Gimpressionist. Uses Line-art-2 option.\n: file:photoeffects_246_02_part10.scm"
     "Eddy Verlinden <eddy_verlinden@hotmail.com>"
     "Eddy Verlinden"
     "2007, juli"
