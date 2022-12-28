4e3a2867f21998c89ecc540f19cf12bcce575312
Karl <63458781+karlhof26@users.noreply.github.com>
2022-12-28 11:02:52 +0000

    Adjusted Cyan color layer.

--- mm1-vintage-look.0.3_0.scm
+++ mm1-vintage-look.0.3_0.scm
@@ -16,7 +16,7 @@
 ; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 ;
 ;
-; Copyright (C) 2008 Michael Maier info[at]mmip.net
+; Copyright (C) 2008 Michael Maier info[at]mmip.net  
 ;
 ; Version 0.1 - Simulating a vintage photo based on this tutorial:
 ;               http://crazymurdock1.deviantart.com/art/Vintage-look-in-Gimp-61841683
@@ -63,14 +63,14 @@
     )
     
     ;Yellow Layer
-    (set! yellow-layer (car (gimp-layer-new img drawable-width drawable-height RGB "Yellow" 100  LAYER-MODE-MULTIPLY)))	
+    (set! yellow-layer (car (gimp-layer-new img drawable-width drawable-height RGB "Yellow" 100  LAYER-MODE-MULTIPLY)))
     (gimp-image-insert-layer img yellow-layer 0 -1)
     (gimp-context-set-background '(251 242 163) )
     (gimp-drawable-fill yellow-layer FILL-BACKGROUND)
     (gimp-layer-set-opacity yellow-layer VarYellow)
     
     ;Magenta Layer
-    (set! magenta-layer (car (gimp-layer-new img drawable-width drawable-height RGB "Magenta" 100  LAYER-MODE-SCREEN)))
+    (set! magenta-layer (car (gimp-layer-new img drawable-width drawable-height RGB "Magenta" 100  LAYER-MODE-ADDITION))) ; was screen
     (gimp-image-insert-layer img magenta-layer 0 -1)
     (gimp-context-set-background '(232 101 179) )
     (gimp-drawable-fill magenta-layer FILL-BACKGROUND)
@@ -79,7 +79,8 @@
     ;Cyan Layer 
     (set! cyan-layer (car (gimp-layer-new img drawable-width drawable-height RGB "Cyan" 100  LAYER-MODE-SCREEN)))
     (gimp-image-insert-layer img cyan-layer 0 -1)
-    (gimp-context-set-background '(9 73 233) )
+    ;(gimp-context-set-background '(9 73 233) )
+    (gimp-context-set-background '(21 244 215) )
     (gimp-drawable-fill cyan-layer FILL-BACKGROUND)
     (gimp-layer-set-opacity cyan-layer VarCyan)
     
@@ -92,18 +93,18 @@
 
 (script-fu-register "mm1-vintage-look"
                     "Vintage Look"
-                    "Simulation of a 70s vintage look.
-\nfile:mm1-vintage-look.0.3_0.scm"
+                    "Simulation of a 70s vintage look.\nfile:mm1-vintage-look.0.3_0.scm"
                     "Michael Maier info[at]mmip.net >" 
                     "(c) Michael Maier. This is GPL Free Software."
                     "March 3, 2008" 
                     "*"
-                    SF-IMAGE      "Image"    0
-                    SF-DRAWABLE   "Drawable" 0
-                    SF-ADJUSTMENT _"Cyan"    '(17 0 100 1 1 0 0)
-                    SF-ADJUSTMENT _"Magenta" '(20 0 100 1 1 0 0)
-                    SF-ADJUSTMENT _"Yellow"  '(59 0 100 1 1 0 0)
-                    SF-TOGGLE     _"Bleach Bypass" TRUE
-                    )
+                    SF-IMAGE      "Image"       0
+                    SF-DRAWABLE   "Drawable"    0
+                    SF-ADJUSTMENT _"Cyan"       '(5 0 100 1 1 0 0)
+                    SF-ADJUSTMENT _"Magenta"    '(18 0 100 1 1 0 0)
+                    SF-ADJUSTMENT _"Yellow"     '(59 0 100 1 1 0 0)
+                    SF-TOGGLE     _"Bleach Bypass"  TRUE
+)
 
-(script-fu-menu-register "mm1-vintage-look" "<Image>/Script-Fu/Artistic")
+(script-fu-menu-register "mm1-vintage-look" "<Toolbox>/Script-Fu/Artistic")
+; end of script
